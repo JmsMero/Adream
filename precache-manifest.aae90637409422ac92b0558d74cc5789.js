7e2011cd52c2266b8bd3982db413de2ef5a9cc8a
caobenfei <caobenfei@zhrt.com.cn>
2020-12-31 14:21:15 +0800

    first commit

---
+++ precache-manifest.aae90637409422ac92b0558d74cc5789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc72d7327e57b3bd695fb9f2be39326c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe54296b4073306c59eb",
+    "url": "/static/css/main.fe9c6214.chunk.css"
+  },
+  {
+    "revision": "bc3ff80211c5c05c7510",
+    "url": "/static/js/2.b209ffe5.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.b209ffe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe54296b4073306c59eb",
+    "url": "/static/js/main.c622251b.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
